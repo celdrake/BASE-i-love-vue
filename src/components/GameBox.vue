56030822944d26c23adecfa1a3879fe7ff701c22
Celia <celdrake@gmail.com>
2019-04-06 11:10:25 +0200

    3. Static matrix in GameBox

--- src/components/GameBox.vue
+++ src/components/GameBox.vue
@@ -1,6 +1,10 @@
 <template>
   <div class="game-box">
-    game box
+    <div v-for="(row, rowIndex) in matrix" :key="rowIndex">
+      <div v-for="(cell, cellIndex) in row" :key="cellIndex">
+        {{ cell.content }}
+      </div>
+    </div>
   </div>
 </template>
 
@@ -8,6 +12,27 @@
 
 export default {
   name: 'GameBox',
+  data() {
+    return {
+      matrix: this.initMatrix(4),
+    };
+  },
+  methods: {
+    initMatrix(gameSize) {
+      const matrix = [];
+      for (let row = 0; row < gameSize; row += 1) {
+        const rowColumns = [];
+        for (let col = 0; col < gameSize; col += 1) {
+          rowColumns.push({
+            content: 'empty', // empty, pattern, success, error
+            display: true,
+          });
+        }
+        matrix.push(rowColumns);
+      }
+      return matrix;
+    },
+  },
 };
 
 </script>
